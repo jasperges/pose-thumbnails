826664d0f000d10cce1e04d65f76bcf0ce5e8440
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2016-09-18 23:23:22 +0200

    UI change + thumbnail sorting test
    Moved the thumbnails to the existing Pose Library panel
    Test for thumbnail sorting:
        When reading dir, it is assumed that the files are in the same order
        as the frames of the poses.
        When drawing, they follow the order of the inidices of the poses.

--- pose_thumbnails.py
+++ pose_thumbnails.py
@@ -73,8 +73,13 @@ def get_pose_thumbnails(self, context):
             number: Unique value used as the identifier for this item (stored
                 in file data). Use when the identifier may need to change.
     '''
+    def sort_thumbnails(thumbnail):
+        pose_markers = context.object.pose_library.pose_markers
+        frame_sorted_pose_markers = sorted(pose_marker, key=lambda pose_marker: pose_marker.frame)
+
+
     enum_items = []
-    if context is None:
+    if context is None or not context.object.pose_library.pose_markers:
         return enum_items
     thumbnail_dir = '/Users/jasperge/projects/prive/blender/scripting/pose_previews_docs/pose_thumbnails/krab_clean/'
     pose_thumbnail_collection = preview_collections['pose_lib']
@@ -82,7 +87,14 @@ def get_pose_thumbnails(self, context):
         return pose_thumbnail_collection.pose_thumbnails
     logger.info("Scanning directory %s..." % (thumbnail_dir))
     image_paths = get_images_from_dir(thumbnail_dir)
-    for i, name in enumerate(image_paths):
+    pose_markers = context.object.pose_library.pose_markers
+    frame_sorted_pose_markers = sorted(pose_markers, key=lambda pose_marker: pose_marker.frame)
+    zipper = zip(frame_sorted_pose_markers, image_paths)
+    sorted_image_paths = sorted(zipper, key=lambda p: pose_markers.find(p[0].name))
+    # sorted_image_paths = sorted(image_paths, key=sort_thumbnails)
+    sorted_image_paths = [img[1] for img in sorted_image_paths]
+    logger.debug(sorted_image_paths)
+    for i, name in enumerate(sorted_image_paths):
         filepath = os.path.join(thumbnail_dir, name)
         frame = context.object.pose_library.pose_markers[i].frame
         display_name = os.path.splitext(name)[0].split('_', 1)[-1]
@@ -112,40 +124,56 @@ def update_pose(self, context):
     return
 
 
-class PoseLibPreviewPanel(bpy.types.Panel):
-    """Creates a Panel in the armature context of the properties editor"""
-    bl_label = "Pose Library Thumbnails"
-    bl_idname = "DATA_PT_pose_thumbnails"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = "data"
-    bl_options = {'DEFAULT_CLOSED'}
+# class PoseLibPreviewPanel(bpy.types.Panel):
+#     """Creates a Panel in the armature context of the properties editor"""
+#     bl_label = "Pose Library Thumbnails"
+#     bl_idname = "DATA_PT_pose_thumbnails"
+#     bl_space_type = 'PROPERTIES'
+#     bl_region_type = 'WINDOW'
+#     bl_context = "data"
+#     bl_options = {'DEFAULT_CLOSED'}
 
-    @classmethod
-    def poll(cls, context):
-        user_prefs = context.user_preferences
-        addon_prefs = user_prefs.addons[__package__].preferences
-        obj = context.object
+#     @classmethod
+#     def poll(cls, context):
+#         user_prefs = context.user_preferences
+#         addon_prefs = user_prefs.addons[__package__].preferences
+#         obj = context.object
 
-        return (obj and
-                obj.type == 'ARMATURE' and
-                obj.pose and
-                addon_prefs.add_3dview_prop_panel)
+#         return (obj and
+#                 obj.type == 'ARMATURE' and
+#                 obj.pose and
+#                 addon_prefs.add_3dview_prop_panel)
 
-    def draw(self, context):
-        user_prefs = context.user_preferences
-        addon_prefs = user_prefs.addons[__package__].preferences
-        show_labels = addon_prefs.show_labels
-        obj = context.object
-        pose = obj.pose
-        pose_lib = obj.pose_library
+#     def draw(self, context):
+#         user_prefs = context.user_preferences
+#         addon_prefs = user_prefs.addons[__package__].preferences
+#         show_labels = addon_prefs.show_labels
+#         obj = context.object
+#         pose = obj.pose
+#         pose_lib = obj.pose_library
 
-        layout = self.layout
-        layout.template_icon_view(
-            obj.pose_library,
-            "pose_thumbnails",
-            show_labels=show_labels,
-            )
+#         layout = self.layout
+#         layout.template_icon_view(
+#             obj.pose_library,
+#             "pose_thumbnails",
+#             show_labels=show_labels,
+#             )
+
+
+def pose_thumbnails_draw(self, context):
+    if not context.object.pose_library.pose_markers:
+        return
+    user_prefs = context.user_preferences
+    addon_prefs = user_prefs.addons[__package__].preferences
+    show_labels = addon_prefs.show_labels
+    obj = context.object
+    layout = self.layout
+    row = layout.row()
+    row.template_icon_view(
+        obj.pose_library,
+        "pose_thumbnails",
+        show_labels=show_labels,
+        )
 
 
 def register():
@@ -153,6 +181,7 @@ def register():
         items=get_pose_thumbnails,
         update=update_pose,
         )
+    bpy.types.DATA_PT_pose_library.prepend(pose_thumbnails_draw)
     # bpy.types.Scene.pose_search = bpy.props.CollectionProperty(
     #     type=PoseLibSearch)
     # bpy.types.Object.pose_previews = EnumProperty(
@@ -185,6 +214,7 @@ def register():
 
 
 def unregister():
+    bpy.types.DATA_PT_pose_library.remove(pose_thumbnails_draw)
     for pose_thumbnail_collection in preview_collections.values():
         bpy.utils.previews.remove(pose_thumbnail_collection)
     preview_collections.clear()
